cc30086c942a06839efaba62e9841d4dd743577e
Yvonne King <von_amari@hotmail.com>
2020-10-28 18:51:05 -0600

    Update contact.php

--- contact.php
+++ contact.php
@@ -91,9 +91,7 @@
             <div class="container">
                 <h2>Contact Us</h2>
                 <p>
-                    Est dolorum ut non facere possimus quibusdam eligendi voluptatem.
-                    Quia id aut similique quia voluptas sit quaerat debitis. Rerum omnis
-                    ipsam aperiam consequatur laboriosam nemo harum praesentium.
+                    Questions? We’re here to serve you!
                 </p>
             </div>
         </div>
